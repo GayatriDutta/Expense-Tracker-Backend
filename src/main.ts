07b6bf5d89d2665944ed2d72556e9e36e63a1520
GayatriDutta <69198031+GayatriDutta@users.noreply.github.com>
2025-10-18 22:24:25 +0530

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,8 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
    app.enableCors({
-    origin: 'http://localhost:5173', // your frontend URL
+    origin: ['http://localhost:5173',
+             'https://expense-tracker-blush-nu-94.vercel.app'],
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
     credentials: true, // if sending cookies or authorization headers
   });
