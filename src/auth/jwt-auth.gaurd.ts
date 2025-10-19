01e47b204779420bb3af23990c5c53aabc82a6a2
Gayatri Dutta <gayatridutta@Mac.lan>
2025-10-19 12:13:51 +0530

    public for register api

--- src/auth/jwt-auth.gaurd.ts
+++ src/auth/jwt-auth.gaurd.ts
@@ -9,13 +9,13 @@ export class JwtAuthGuard extends AuthGuard('jwt') {
     super();
   }
 
-  canActivate(context: ExecutionContext) {
+    canActivate(context: ExecutionContext) {
     const isPublic = this.reflector.getAllAndOverride<boolean>(IS_PUBLIC_KEY, [
       context.getHandler(),
       context.getClass(),
     ]);
     if (isPublic) {
-      console.log('Public route accessed, skipping JWT guard'); // <-- log for public routes
+      console.log('✅ Public route accessed – skipping JWT auth');
       return true;
     }
     return super.canActivate(context);
--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,15 @@
-import { NestFactory } from '@nestjs/core';
+import { NestFactory, Reflector } from '@nestjs/core';
 import { ValidationPipe } from '@nestjs/common';
 import { AppModule } from './app.module';
+import { JwtAuthGuard } from './auth/jwt-auth.gaurd';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
+
+  const reflector = app.get(Reflector);
+  app.useGlobalGuards(new JwtAuthGuard(reflector));
    app.enableCors({
     origin: 'https://expense-tracker-blush-nu-94.vercel.app',
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
@@ -12,7 +18,5 @@ async function bootstrap() {
 
   const port = process.env.PORT || 5000;
   await app.listen(port);
-
-  app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
 }
 bootstrap();
